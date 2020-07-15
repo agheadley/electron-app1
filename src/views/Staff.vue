7188712082be55f83683659ef287d00b9b61fb89
agheadley <agheadley@gmail.com>
2020-07-15 07:52:07 +0100

    added file download check

--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -3,8 +3,7 @@
 
 
 
-
-
+<div><button @click="getDownload">DOWNLOAD FILE</button></div>
 
 
 
@@ -13,9 +12,8 @@
 </template>
 
 <script>
-  
 
-//import * as csv from './../scripts/csv'
+import * as request from './../scripts/request';
 
 export default {
 name: 'Block',
@@ -26,7 +24,25 @@ data() {
 },
 methods: {
     
-   
+    getDownload() {
+        fetch(request.url+'generateDownload', {  
+            method: 'POST',  
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({data:'test,test2,test3\nx,y,z'})
+        }).then(res => res.json())
+        .then (data=>{              
+            this.message=data;
+            console.log(data);
+            console.log(data.response)
+            if(data.response==="SUCCESS") {
+                let x=request.download('out.csv');
+                console.log(x);
+            }
+        }).catch(err=>{
+            this.message=err;
+            console.log(err);
+        })
+    },
 
 
 }
